14d53b2836db8f57ea54a6f5abce816c4d434ce9
Valua-s <speranskaya.valya@yandex.ru>
2024-04-27 09:59:59 +0500

    first commit

--- yatube_api/posts/admin.py
+++ yatube_api/posts/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from posts.models import Comment, Follow, Group, Post
 
-# Register your models here.
+admin.site.register(Post)
+admin.site.register(Group)
+admin.site.register(Comment)
+admin.site.register(Follow)
